3dd3db7ca8f9652b0377c04545c569c755ee8432
elranborohovich <eb161085@gmail.com>
2026-05-26 02:33:36 +0300

    עדכון תרגיל 2

---
+++ p.22/2.py
@@ -0,0 +1,8 @@
+# מחרוזת
+text="jerusalem"
+#א. חיתוך מהתו השלישי ועד הסוף
+cut_text = text[2:]
+# ב. הדפסת אורך המחרוזת
+print(len(text))
+# ג. החלפת כל הרווחים במקף
+text = text.replace(" ", "-")            
