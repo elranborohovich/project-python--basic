fae87239d02a0821cadde2565ef14a89068d9972
elranborohovich <eb161085@gmail.com>
2026-05-26 02:28:47 +0300

    p.15/22

---
+++ p.15/1.py
@@ -0,0 +1,8 @@
+james=30
+jon=20
+mike=10
+avg=(james+jon+mike)/3
+# option 1
+print("The average is",avg)
+# option 2
+print(f"The average is {avg}")
---
+++ p.22/1.py
@@ -0,0 +1,8 @@
+# מחרוזת
+text="jerusalem"
+# הדפסת האות הראשונה
+print(text[0])
+# הדפסת האות האחרונה
+print(text[-1])
+# הדפסת האות האמצעית
+print(text[len(text)//2])
